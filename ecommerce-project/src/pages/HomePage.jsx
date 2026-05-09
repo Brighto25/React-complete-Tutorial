198b64f8d2d47a45a3cb8a8e0c85df4406e5730c
Brighto25 <Bright.Addaneyf3@gmail.com>
2026-05-09 00:11:12 +0000

    Lift state up and share cart between homepage and checkout page

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -5,15 +5,13 @@ import "./HomePage.css";
 
 export function HomePage() {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+
   useEffect(() => {
     axios.get("/api/products").then((response) => {
       setProducts(response.data);
     });
 
-    axios.get("/api/cart-items").then((response) => {
-      setCart(response.data);
-    }); 
+   
   }, []);
 
   return (
