7084b48f0928e83f65050e0bacc3d86917bfa4a3
Brighto25 <Bright.Addaneyf3@gmail.com>
2026-05-09 00:28:41 +0000

    Lift state error handled

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
 import "./HomePage.css";
 
-export function HomePage() {
+export function HomePage({cart}) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
